58eff1fcf4aa187e113288f697a16a09681aa22a
HyunSoo Kim <39149858+dblepart99@users.noreply.github.com>
2021-07-06 21:53:08 +0900

    Update boj_10871.cpp

--- BOJ/0706/boj_10871.cpp
+++ BOJ/0706/boj_10871.cpp
@@ -1,26 +1,17 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 int main(){
     
-    int score;
-    cin >> score;
-    if(score >= 90 && score <=100){
-        cout << "A";
-        return 0;
-    }else if(score >= 80 && score <= 89){
-        cout << "B";
-        return 0;
-    }else if(score >= 70 && score <= 79){
-        cout << "C";
-        return 0;
-    }else if(score >= 60 && score <= 69){
-        cout << "D";
-        return 0;
-    }else{
-        cout << "F";
-        return 0;
+    int N, target, tmp;
+    cin >> N >> target;
+    
+    for(int i = 0; i < N; i++){
+        cin >> tmp;
+        if(tmp < target){
+            cout << tmp << " ";
+        }
     }
     
 }
